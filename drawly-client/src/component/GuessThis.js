3626457d6e8aee6825c7e700fdb91b9dd1c18a5c
TAHA202020 <tahajayche@gmail.com>
2025-03-20 17:54:17 +0100

    add css

--- drawly-client/src/component/GuessThis.js
+++ drawly-client/src/component/GuessThis.js
@@ -3,6 +3,6 @@ export default function GuessThis({wordLenght})
     return (<div className="flex justify-center items-center flex-col relative">
         <p className="text-sm font-normal">Guess This :</p>
         <p className="font-black text-xl tracking-widest">{"_".repeat(wordLenght)}</p>
-        <p className="absolute top-[10%] right-0 translate-x-[150%] text-xs">{wordLenght}</p>
+        <p className="absolute top-[40%] right-0 translate-x-[150%] text-xs">{wordLenght}</p>
     </div>)
 }
